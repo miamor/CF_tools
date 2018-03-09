c3a624f017eab89bba61bff737a81a328fb43891
Tu Nguyen <tunguyenhsgs@gmail.com>
2018-03-09 13:08:38 +0700

    09.03.18

--- jobs/SSD_VOC0712_300x300/VGG_VOC0712_SSD_VOC0712_300x300.sh
+++
@@ -1,4 +0,0 @@
-/home/tunguyen/CAFFE/build/tools/caffe train \
---solver="/home/tunguyen/CF_tools/models/SSD_VOC0712_300x300/solver.prototxt" \
---snapshot="/home/tunguyen/CF_tools/snapshot_models/SSD_VOC0712_300x300/VGG_VOC0712_SSD_VOC0712_300x300_iter_23.solverstate" \
---gpu 0 2>&1 | tee /home/tunguyen/CF_tools/jobs/SSD_VOC0712_300x300/VGG_VOC0712_SSD_VOC0712_300x300_20180309091401.log
--- jobs/VGGNet/VOC0712/SSD_300x300_orig/VGG_VOC0712_SSD_300x300_orig.sh
+++
@@ -1,5 +0,0 @@
-cd /media/ps/AAA/CF_tools
-./build/tools/caffe train \
---solver="models/VGGNet/VOC0712/SSD_300x300_orig/solver.prototxt" \
---weights="models/VGGNet/VGG_ILSVRC_16_layers_fc_reduced.caffemodel" \
---gpu 0,1,2,3 2>&1 | tee jobs/VGGNet/VOC0712/SSD_300x300_orig/VGG_VOC0712_SSD_300x300_orig.log
